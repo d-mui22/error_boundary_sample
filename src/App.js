d67e12055989a321ccef494b7af317b9390cff76
d.mui2383 <d.mui2383@gmail.com>
2019-01-19 11:57:19 -0500

    initial app setup completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Persons/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {id: 'abc123', name: 'Dave', age: 35},
+      {id: 'cba321', name: 'Tina', age: 31},
+      {id: 'adsf12', name: 'Blah', age: 33},
+    ],
+    otherState: 'something else',
+    showPersons: false
+  }
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id
+    })
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons]
+    persons.splic(personIndex, 1)
+    this.setState({
+      persons: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const willShow = this.state.showPersons;
+    this.setState({
+      showPersons: !willShow
+    })
+  }
+
   render() {
+    let persons = null
+    let btnClass = ''
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            )
+          })}
+        </div>
+      )
+    }
+
+    const assignedClasses = []
+
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push('red')
+    }else if (this.state.persons.length <= 1) {
+      assignedClasses.push('bold')
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
---
+++ src/components/Persons/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Person = (props) => {
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h3>{props.age}</h3>
+    </div>
+  )
+}
+
+export default Person
