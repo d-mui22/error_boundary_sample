b24514de14c7823664952b939a7852a78de00a09
d.mui2383 <d.mui2383@gmail.com>
2019-01-20 15:58:22 -0500

    Cockpit component finished. dynamic CSS added to persons

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Cockpit = (props) => {
+  let persons = null
+
   return (
     <div>
       <h1>React File Structure Sample</h1>
       <button onClick={props.toggle}>Toggle Persons</button>
+      {persons}
     </div>
   )
 }
