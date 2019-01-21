58b2c7cd07e227cc3af5b73c617dc61550ae22d5
d.mui2383 <d.mui2383@gmail.com>
2019-01-21 14:09:16 -0500

    error boundary component setup completed. wrapped around Persons map

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Person from './Person/Person'
-
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 const Persons = (props) => ( // es6 syntax if no logic statements can omit return
   props.persons.map((person, index) => { // () needed in map when more than one arg
-    return <Person
+    return <ErrorBoundary key={person.id}>
+      <Person
       key={person.id}
       deleteHandler={() => {props.deleteHandler(index)}}
       name={person.name}
       age={person.age}
       nameChange={(event) => {props.nameChangeHandler(event, person.id)}}
-    />
+      />
+    </ErrorBoundary>
   })
 )
 
